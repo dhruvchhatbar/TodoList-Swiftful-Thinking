dcb5a24d63953a8803989054ecb5981dac9a1bbb
Dhruv <100026417+dhruvchhatbar@users.noreply.github.com>
2026-04-21 15:26:17 +0530

    initial commit

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dhruv Chhatbar on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
